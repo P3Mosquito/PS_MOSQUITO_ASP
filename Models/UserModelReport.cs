d77ac3cc35b6f003990c318d600f774516ef2c67
RolyCalleGutierrez1 <roly.calle@trendstechnologies.com>
2023-11-10 09:28:25 -0400

    REPORTE COMPLEATADO CONTROLADOR Y FORNTEND

---
+++ Models/UserModelReport.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ps_mosquito_asp.Models
+{
+    public class UserModelReport
+    {
+        public string? id { get; set; }
+        public string? name { get; set; }
+        public string? lastname { get; set; }
+        public string? taskName { get; set; }
+        public string? ci { get; set; }
+        public string? role { get; set; }
+        public string? position { get; set; }
+        [Required]
+        public string? email { get; set; }
+        [Required]
+        public string? password { get; set; }
+    }
+}
